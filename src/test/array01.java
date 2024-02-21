9140e510bf322d6487ff8e7b598e1c0332181a87
SSong-sh <tjdgp0817@gmail.com>
2024-02-21 14:05:28 +0900

    백준 접미사 배열 문제 풀이

--- src/test/array01.java
+++ src/test/array01.java
@@ -1,13 +1,34 @@
 package test;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array01 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		
 		String str = scanner.nextLine();
+		String substrings [] = new String [str.length()];  //문자의 길이를 배열로 만들기
+		
+		/* String substring > 문자열 자르기 사용 > 시작 위치 인덱스 포함, 끝 인덱스 포함 x
+		 *  i = 0 substring의 1번 = baekjoon
+		 *  ...
+		 *  i = 7 substring의 7번 = n
+		 */ 
+		for (int i = 0; i < str.length(); i++) {
+			substrings[i] = str.substring(i);
+		}
+		
+		/* Arrays.sort(Object[] a) > ascending order 
+		 * Arrays.sort(배열, Collections.reverseOrder()) > 내림차순 
+		 */
+		Arrays.sort(substrings); 
+
+		// 배열에 저장된 부분 문자열 출력
+		for (String substringStr : substrings) {
+			System.out.println(substringStr);
+		}
+
 	}
 
 }
