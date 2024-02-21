745e24aaa4450df49d0239fdc17332a9acbbb818
SSong-sh <tjdgp0817@gmail.com>
2024-02-21 10:34:19 +0900

    remote 테스트

---
+++ src/test/array01.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Scanner;
+
+public class array01 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String str = scanner.nextLine();
+	}
+
+}
