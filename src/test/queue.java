f68ffa35c411e28ce7f0a3e2b32c3911758d2f9c
SSong-sh <tjdgp0817@gmail.com>
2024-03-04 07:55:55 +0900

    백준, 프로그래머스 큐 문제 완성

--- src/test/queue.java
+++ src/test/queue.java
@@ -4,40 +4,66 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class queue {
-	public static void main(String[] args) throws IOException {
+	
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-		for(int k = 0; k < T; k++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int k = 0; k < T; k++) {
+        	
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
 
-			LinkedList<int[]> queue = new LinkedList<>();
+            Queue<int[]> queue = new LinkedList<>(); // Queue 인터페이스를 사용
 
-			st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
 
-			for (int i = 0; i < N; i++) {
-				queue.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
-			}
-			
-			System.out.println();
-			// 큐에 들어있는 원소와 우선순위를 출력
-			System.out.println("Queue contents:");
-			for (int[] element : queue) {
-			    System.out.println("Index: " + element[0] + ", Priority: " + element[1]);
-			}
+            for (int i = 0; i < N; i++) {
+                queue.offer(new int[]{i, Integer.parseInt(st.nextToken())});
+            }
 
-			
-			
-		}
-	}
+            int count = 0;
 
+            while (!queue.isEmpty()) {
+            	
+                int[] first = queue.poll();
+                boolean isMax = true;
+
+                for (int[] element : queue) {
+                	
+                    if (first[1] < element[1]) {
+                    	
+                        queue.offer(first);
+                        isMax = false;
+                        break;
+                    }
+                    
+                }
+
+                if (!isMax) {
+                	continue;
+                }
+
+                count++;
+                
+                if (first[0] == M) {          
+                	break;
+                }
+
+            }
+            
+            sb.append(count).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
 }
--- src/test/queue01.java
+++ src/test/queue01.java
@@ -1,5 +1,34 @@
 package test;
+import java.util.*;
+
 
 public class queue01 {
+	    public int solution(int[] priorities, int location) {
+	        int answer = 0;
+	        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+	        Queue<int[]> queue = new LinkedList<>();
+
+	        for (int i = 0; i < priorities.length; i++) {
+	            pq.offer(priorities[i]);
+	            queue.offer(new int[]{i, priorities[i]});
+	        }
+
+	        while (!queue.isEmpty()) {
+	            int[] first = queue.poll();
+
+	            if (first[1] == pq.peek()) {
+	                pq.poll();
+	                answer++;
+
+	                if (first[0] == location)
+	                    break;
+	            } else {
+	                queue.offer(first);
+	            }
+	        }
+
+	        return answer;
+	    }
+	
 
 }
