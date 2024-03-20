fbfc8bface54728ca8a32c7b03db7ee454a74d5f
SSong-sh <tjdgp0817@gmail.com>
2024-03-20 15:06:46 +0900

    브루프 포스- 퇴사  풀이 완료 + 주석 처리

--- src/baekjoon/Main_Brute.java
+++ src/baekjoon/Main_Brute.java
@@ -25,10 +25,14 @@ public class Main_Brute {
 		}
 		
 		//N일에 얻을 수 있는 최대 수익
+		//DP[현재 날의 상담 기간을 계산했을 때 끝나는 날]
+		//n+1인 이유 : 돈은 그 다음날 받기 때문에
 		int[] dp = new int[n+1];		
 		
 		for (int i=0; i<n; i++) {
 			if (i + t[i] <= n) { 
+				//max(DP[현재 날의 상담 기간을 계산했을 때 끝나는 날], DP[현재 날까지 계산된 값] + pay[현재 날 상담을 통해 얻는 값])
+				//지금까지 계산한 값 vs 지금 해야할 계산
 				dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
 			}
 
