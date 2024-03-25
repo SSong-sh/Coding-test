d04e5eee800dccf3dad10825eb37041396928764
SSong-sh <tjdgp0817@gmail.com>
2024-03-26 08:12:54 +0900

    재귀 문제(합병정렬) 풀이완

--- src/baekjoon/Main_Recursive.java
+++ src/baekjoon/Main_Recursive.java
@@ -6,12 +6,78 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_Recursive {
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder builder = new StringBuilder();
-		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-	}
 
+    private static int[] numArr;
+    private static int[] copyArr;
+    private static int K;
+    static int count;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        int N = Integer.parseInt(tokenizer.nextToken());
+        K = Integer.parseInt(tokenizer.nextToken());
+
+        numArr = new int[N];
+        copyArr = new int[N];
+
+
+        tokenizer = new StringTokenizer(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            numArr[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        mergeSort(0, numArr.length - 1);
+
+        if (count < K) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result);
+        }
+        
+    }
+
+    private static void mergeSort(int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+            merge(left, mid, right);
+        }
+    }
+
+    private static void merge(int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (numArr[i] <= numArr[j]) {
+                copyArr[k++] = numArr[i++];
+            } else {
+                copyArr[k++] = numArr[j++];
+            }
+        }
+
+        while (j <= right) {
+            copyArr[k++] = numArr[j++];
+        }
+
+        while (i <= mid) {
+            copyArr[k++] = numArr[i++];
+        }
+
+        // 병합 후의 배열을 다시 numArr로 복사
+        for (int index = left; index <= right; index++) {
+            numArr[index] = copyArr[index];
+            count++; // 복사될 때마다 count 증가
+            if (count == K) {
+                result = numArr[index];
+                return;
+            }                          
+        }
+    }
 }
